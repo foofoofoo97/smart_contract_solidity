12ad494f7c1b3f1430815b5ae732aad64dc52c12
foofoofoo <fooziqin@gmail.com>
2021-09-20 00:21:32 +0800

    commit

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -14,15 +14,15 @@ const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
   console.log("Attempting to deploy from acc ", accounts[0]);
 
-  //   const result = await new web3.eth.Contract(JSON.parse(interface))
-  //     .deploy({ data: bytecode, arguments: ["Hi There!"] })
-  //     .send({
-  //       gas: "1000000",
-  //       gasPrice: "5000000000",
-  //       from: accounts[0],
-  //     });
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi There!"] })
+    .send({
+      gas: "1000000",
+      gasPrice: "5000000000",
+      from: accounts[0],
+    });
 
-  //   console.log("Contract deployed to", result.options.address);
+  console.log("Contract deployed to", result.options.address);
 };
 
 deploy();
